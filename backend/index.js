be6c3c28bcefcbf3917bfa4887810b50ecc35281
payalkherajani <payal.kherajani19@gmail.com>
2021-08-27 11:22:23 +0530

    backend:mongodb-connection-made

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./db/database')
 
 
 const app = express()
 
 app.use(express.json())
 app.use(cors());
+connectDB()
 
 const PORT = process.env.PORT || 5000;
 

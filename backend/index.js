39344b5fd08d1d74f400a691f75a45da3cdb0808
payalkherajani <payal.kherajani19@gmail.com>
2021-08-28 10:44:45 +0530

    backend:routes-setup

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./db/database')
-
+const userRoutes = require('./routes/users.route')
+const postRoutes = require('./routes/posts.route')
 
 const app = express()
 
@@ -10,6 +11,9 @@ app.use(cors());
 connectDB()
 
 const PORT = process.env.PORT || 5000;
+
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server Started on PORT ${PORT}`)
